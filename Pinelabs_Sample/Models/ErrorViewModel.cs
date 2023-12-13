afa90e436f2f99bd66b7e6c60a0aea3f759b5396
Kartik <kartik20044@gmail.com>
2023-12-13 13:32:59 +0530

    code replaced

--- Pinelabs_Sample/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Pinelabs_Sample.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
